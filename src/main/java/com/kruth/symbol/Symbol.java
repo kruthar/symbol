410496074aea882081c1f3802e74d6fe1824bc9e
Andy Kruth <kruthar@gmail.com>
2016-02-23 14:18:56 -0600

    stubbed in a maven project to tinker with interpreter concepts

---
+++ src/main/java/com/kruth/symbol/Symbol.java
@@ -0,0 +1,10 @@
+package com.kruth.symbol;
+
+/**
+ * Created by kruthar on 2/23/16.
+ */
+public class Symbol {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
